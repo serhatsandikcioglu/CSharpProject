1729f372048efa847104ede1520e5a989b18f3d9
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-04-28 11:29:07 +0300

    Add files via upload

---
+++ KarakterTerstenYazdırma/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bir Cümle Giriniz: ");
+string cumle = Console.ReadLine();
+string[] kelimeler = cumle.Split(' ');
+
+for (int i = 0; i < kelimeler.Length; i++)
+{
+    char[] harfler = kelimeler[i].ToCharArray();
+    Array.Reverse(harfler);
+    kelimeler[i] = new string(harfler);
+}
+
+string tersCumle = string.Join(" ", kelimeler);
+Console.WriteLine(tersCumle);
