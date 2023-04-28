f384a9b9cf44cbf4e75fd3344387b8d3eb9230f5
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-04-28 11:10:26 +0300

    Add files via upload

---
+++ Algoritma/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Bir kelime ve bir sayı giriniz: ");
+string input = Console.ReadLine();
+string[] parts = input.Split(",");
+string word = parts[0];
+int number = int.Parse(parts[1]);
+string newword = word.Remove(number, 1);
+Console.WriteLine(newword);
