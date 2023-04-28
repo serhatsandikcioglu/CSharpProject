d07f8f88651281ded5e81ec4166d3f5c0cc1c891
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-04-28 11:00:52 +0300

    Add files via upload

---
+++ DaireÇizme/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Dairenin yarıçapını girin: ");
+int radius = int.Parse(Console.ReadLine());
+for (int i = -radius; i <= radius; i++)
+{
+    for (int j = -radius; j <= radius; j++)
+    {
+        if (i * i + j * j <= radius * radius)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
