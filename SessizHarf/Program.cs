d42690c6eb2295f78a00176a4d9fe803ba511125
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-05-02 10:49:11 +0300

    Add files via upload

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+
+Console.WriteLine("Cümle giriniz: ");
+string input = Console.ReadLine();
+string[] kelimeler = input.Split(' ');
+string unsuz = "bcçdfgğhjklmnprsştvyz";
+int ikili = 0;
+
+foreach (var item in kelimeler)
+{
+    bool kontrol = false;
+
+    for (int i = 0; i < item.Length - 1; i++)
+    {
+        if (unsuz.Contains(item[i]) && unsuz.Contains(item[i + 1]))
+        {
+            kontrol = true; break;
+        }
+
+    }
+    Console.Write(kontrol + " ");
+}
