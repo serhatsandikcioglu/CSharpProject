094f11361f815c3af9f3f20eb1e487aa7752d864
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-05-02 10:17:00 +0300

    Add files via upload

---
+++ TersKelimeYazdırma/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Cümle giriniz: ");
+string input = Console.ReadLine();
+string[] kelimeler = input.Split(' ');
+for (int i = 0; i < kelimeler.Length; i++)
+{
+    char[] chars = kelimeler[i].ToCharArray();
+    Array.Reverse(chars);
+    kelimeler[i] = new string(chars);
+}
+Console.WriteLine(string.Join(" ", kelimeler));
