d2208a4f2f8f2c5d3c5b10444f6af68860c0c8b7
Serhat Sandıkçıoğlu <124772332+serhatsandikcioglu@users.noreply.github.com>
2023-05-22 13:39:55 +0300

    Add files via upload

---
+++ BarcodeApp/BarcodeActions.cs
@@ -0,0 +1,39 @@
+﻿using BarcodeLib;
+using IronBarCode;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+
+namespace BarcodeApp
+{
+    public class BarcodeActions
+    {
+        static TYPE type = TYPE.PHARMACODE;
+        public static Barcode CreateAndSaveBarcode(string data)
+        {
+            Barcode barcode = new Barcode(data, type);
+            barcode.Encode(type, data);
+            barcode.SaveImage(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\barcode.png", BarcodeLib.SaveTypes.PNG);
+            Console.WriteLine("The barcode was created and saved, the stored data -> {0}", data);
+            return barcode;
+        }
+        public static string ReadBarcode(string imagePath)
+        {
+            var barcodeReader = BarcodeReader.QuicklyReadOneBarcode(imagePath);
+            if (barcodeReader != null)
+            {
+                return barcodeReader.Text;
+                Console.WriteLine($"Data Value: {barcodeReader.Text}");
+            }
+
+            return null;
+        }
+    }
+}
+
---
+++ BarcodeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using BarcodeLib;
+using BarcodeApp;
+using System.Diagnostics;
+int input;
+do
+{
+    Console.WriteLine("Please select the operation you want to perform : \n*************************\n(1) Create Barcode\n(2) ReadBarcode\n" +
+        "(0) Exit");
+    input = int.Parse(Console.ReadLine());
+    switch (input)
+    {
+        case 1:
+            Console.Write("Enter Barcode Data: ");
+            string data = Console.ReadLine();
+            Barcode barcode = BarcodeActions.CreateAndSaveBarcode(data);
+            break;
+        case 2:
+            BarcodeActions.ReadBarcode(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\barcode.png");
+            break;
+        case 0:
+            Console.WriteLine("Exit");
+            break;
+        default:
+            Console.WriteLine("Invalid transaction number, try again.");
+            break;
+    }
+} while (input != 0);
